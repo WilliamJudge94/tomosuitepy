9c92571c4e616802369a60fc041cf2bbf3ad9243
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-09-20 23:12:26 -0500

    adding gpu support/selection

--- tomosuitepy/easy_networks/dain/data_prep.py
+++ tomosuitepy/easy_networks/dain/data_prep.py
@@ -136,7 +136,7 @@ def create_prj_mp4(basedir, video_type='input', types='base', sparse_angle_remov
     return prj_data, np.asarray(out_data)
     
     
-def rife_predict(basedir, location_of_rife=rife_path, exp=2, scale=1.0, video_input_type='input', video_output_type='predicted'):
+def rife_predict(basedir, location_of_rife=rife_path, exp=2, scale=1.0, gpu='0', video_input_type='input', video_output_type='predicted'):
     """Use the neural network called RIFE to upscale the amount of projections.
     
     Parameters
@@ -165,9 +165,10 @@ def rife_predict(basedir, location_of_rife=rife_path, exp=2, scale=1.0, video_in
     second = f'--exp={exp}'
     third = f'--video={basedir}dain/video/{video_input_type}.mp4'
     fourth = f'--scale={scale}'
+    fourth_inter = f'--gpu={gpu}'
     fifth = f"--output={basedir}dain/video/{video_output_type}.mp4"
     
-    print(f"{pre} {first} {second} {third} {fourth} {fifth}")
+    print(f"{pre} {first} {second} {third} {fourth} {fourth_inter} {fifth}")
     
     
 def obtain_frames(basedir, video_type='predicted', return_frames=False, output_folder='frames'):
