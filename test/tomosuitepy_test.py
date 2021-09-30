b52036aaa2d7118f36136880ddea79e8a6ca5f2e
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-09-29 21:24:00 -0500

    Adding second test

--- test/tomosuitepy_test.py
+++ test/tomosuitepy_test.py
@@ -22,6 +22,22 @@ class TestEnv(unittest.TestCase):
         self.assertTrue(os.path.exists(data_path))
         self.assertTrue(np.array_equal(np.load(data_path), np.arange(0, 10)))
 
+        remove_saved_prj_ds_chunk(iteration)
+
+    def test_remove_saved_prj_ds_chunk(self):
+
+        data = np.arange(0, 10)
+        iteration = 0
+
+        save_prj_ds_chunk(data, iteration)
+
+        data_path = f'{os.getcwd()}/tomsuitepy_downsample_save_it_{str(iteration).zfill(4)}.npy'
+
+        remove_saved_prj_ds_chunk(iteration)
+
+        self.assertFalse(os.path.exists(data_path))
+        
+
 
 
 if __name__ == '__main__':
