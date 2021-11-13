abce6403e1c503ee93ac03a82b5209a5ff23bb50
WilliamJudge94 <37912237+WilliamJudge94@users.noreply.github.com>
2021-11-13 12:56:26 -0600

    FMT: pep8 formatting

--- tomosuitepy/methods/denoise_type1/denoise_t1_dataprep.py
+++ tomosuitepy/methods/denoise_type1/denoise_t1_dataprep.py
@@ -6,6 +6,7 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from ...easy_networks.tomogan.data_prep import format_data_tomogan, setup_data_tomogan
 
+
 def start_pbar(prj):
     widgets = ['Progress: ', Percentage(), ' ', Bar(marker='-', left='[', right=']'),
                ' ', Timer(), '  ', ETA(), ' ', FileTransferSpeed()]  # see docs for other options
@@ -14,52 +15,55 @@ def start_pbar(prj):
     pbar.start()
     return pbar
 
+
 def add_poisson_noise(prj, variable):
-    
+
     pbar = start_pbar(prj)
-    
-    vect_func = np.vectorize(add_train_noise, otypes=[list], excluded=['array', 'nothing', 'pbar'])
+
+    vect_func = np.vectorize(add_train_noise, otypes=[list], excluded=[
+                             'array', 'nothing', 'pbar'])
     varss = np.arange(len(prj))
-    varss2 = variable* np.ones(len(varss))
+    varss2 = variable * np.ones(len(varss))
 
     out = vect_func(varss, varss2, array=prj, pbar=pbar, nothing=False)
-    
+
     new_out = []
     for value in out:
         new_out.append(value[0])
-        
+
     return np.asarray(new_out)
+
 
 def add_train_noise(idx, VARIABILITY, array, pbar, nothing=False):
     """Add random poisson noise to an image
-    
+
     Parameters
     ----------
     idx : int
         the index of the array the User would like to add noise to
-        
+
     VARIABILITY : int
         a value passed to the np.poisson scaling
     array : np.array
         2D numpy image
-        
+
     pbar : progressbar
         the progressbar to let the User know something is working
-        
+
     nothing : bool
         if the User would like to return the input value with no noise added
     Returns
     -------
     An array with poisson noise added or the exact same array. Depends on the value of 'nothing'
     """
-    
+
     img = array[idx]
-    
+
     pbar.update(idx)
-    
+
     if nothing:
         return [img, idx]
-    
+
     mins = np.min(img)
     maxs = np.max(img)
 
@@ -69,38 +73,39 @@ def add_train_noise(idx, VARIABILITY, array, pbar, nothing=False):
 
     return np.asarray([img, idx], dtype=object)
 
+
 def fake_noise_test(basedir, noise=125, image_step=20, plot=True, idx=0, figsize=(10, 10), clim=None):
     """A way to quickly see what the noise value does to the data.
-    
+
     Parameters
     ----------
     basedir : str
         the path to the experiment
-        
+
     noise : int
         parameter used to create the poisson noise
-        
+
     image_step : int
         take every image_step image
-        
+
     plot : bool
         if True the idx value will be plotted
-        
+
     idx : int
         the noise_image index to plot
-    
+
     figsize : tup
         figsize passed into plt.figure()
-        
+
     Returns
     -------
     The noisy data
     """
-    
+
     data = loading_tiff_prj(f'{basedir}extracted/projections/')
     data = data[::image_step]
-    noise_data = add_poisson_noise(data, noise)  
-    
+    noise_data = add_poisson_noise(data, noise)
+
     if plot:
         plt.figure(figsize=figsize)
         im = plt.imshow(data[idx])
@@ -109,7 +114,7 @@ def fake_noise_test(basedir, noise=125, image_step=20, plot=True, idx=0, figsize
             plt.clim(clim)
         plt.colorbar(im)
         plt.show()
-        
+
         plt.figure(figsize=figsize)
         plt.imshow(noise_data[idx])
         if clim is not None:
@@ -117,7 +122,7 @@ def fake_noise_test(basedir, noise=125, image_step=20, plot=True, idx=0, figsize
         plt.title('Noisy PRJ')
         plt.colorbar(im)
         plt.show()
-        
+
         plt.figure(figsize=figsize)
         plt.title('Difference')
         image = np.subtract(data[idx], noise_data[idx])
@@ -125,23 +130,23 @@ def fake_noise_test(basedir, noise=125, image_step=20, plot=True, idx=0, figsize
         plt.colorbar()
         plt.title((image.min(), image.max()))
         plt.show()
-        
+
     if not plot:
         return data[idx], noise_data[idx]
-    
-    
+
+
 def setup_fake_noise_train(basedir, noise=125, interval=5, dtype=np.float32):
     """Prepares data for TomoGAN. Please use tomosuite.noise_test_tomogan to set the correct noise value.
-    
+
     Parameters
     ----------
     basedir : str
         the path to the current experiment
-    
+
     nosie : int
         integer value to be passed into the add_poisson_noise() function.
         Use tomosuite.noise_test_tomogan() to find correct noise level
-        
+
     Returns
     -------
     Nothing
@@ -149,34 +154,31 @@ def setup_fake_noise_train(basedir, noise=125, interval=5, dtype=np.float32):
 
     data = loading_tiff_prj(f'{basedir}extracted/projections/')
     noise_data = add_poisson_noise(data, noise)
-    
+
     xtrain, ytrain, xtest, ytest = format_data_tomogan(clean_data=data,
                                                        noisy_data=noise_data,
                                                        interval=interval,
                                                        dtype=dtype)
-        
+
     setup_data_tomogan(basedir=basedir,
                        xtrain=xtrain,
                        ytrain=ytrain,
                        xtest=xtest,
                        ytest=ytest,
                        types='noise')
-    
-    
-    
+
+
 def setup_fake_noise_predict(basedir):
     """Obtain the data to be passed into the TomoGAN predict function
-    
+
     Parameters
     ----------
     basedir : str
         the path to the current project
-    
+
     Returns
     -------
     The current projection data as a np.array
     """
     data = loading_tiff_prj(f'{basedir}extracted/projections/')
     return np.asarray(data)
-
-    
